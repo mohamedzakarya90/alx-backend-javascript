cfac423bf2abd2809ba4a8d8efcd6fb0e7496de4
mohamed zakarya mohamed farag <mohamed.zakrya.farag@gmail.com>
2024-11-26 15:42:04 +0000

    0x05-Node_JS_basic _1

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,15 @@
+/**
+ * logging message to console
+ * @param {string} message -> message to be logged
+ * @returns {void}
+ */
+// The arrow function
+const displayMessage = (message) => console.log(message);
+
+/**
+ * exporting The displayMessage function
+ * @module displayMessage
+ * @function
+ */
+// The default module exportting 
+module.exports = displayMessage;
